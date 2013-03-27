de84def2fa8329ecfa8109645fc63b6c19ea89cc
Joseph Bass <jab416171@gmail.com>
2013-03-26 18:05:51 -0600

    Now the gun (usually) points where the radar points

--- robots/me/joebass/Robot1.java
+++ robots/me/joebass/Robot1.java
@@ -15,25 +15,10 @@ public class Robot1 extends AdvancedRobot
 	 * run: Robot1's default behavior
 	 */
 	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		setAdjustRadarForGunTurn(false);
-		// Robot main loop
-
+		setColors(Color.red,Color.blue,Color.green);
+		setAdjustRadarForGunTurn(true);
 		while(true) {
 			turnRadarRightRadians(Double.POSITIVE_INFINITY);
-			// Replace the next 4 lines with any behavior you would like
-//			ahead(100);
-//			turnGunRight(360);
-//			back(100);
-//			turnGunRight(360);
-
-
 			scan();
 		}
 	}
@@ -42,36 +27,25 @@ public class Robot1 extends AdvancedRobot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
 		double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
-//		System.out.println("Turning radar to: " + Utils.normalRelativeAngle(radarTurn) * (180 / Math.PI));
-//		System.out.println("Radar currently is: " + this.getRadarHeadingRadians() * (180 / Math.PI));
-//		System.out.println("Gun currently is: " + this.getGunHeadingRadians() * (180 / Math.PI));
+		System.out.println("Turning radar to: " + Utils.normalRelativeAngle(radarTurn) * (180 / Math.PI));
+		System.out.println("Radar currently is: " + this.getRadarHeadingRadians() * (180 / Math.PI));
 
 		setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
-//		setTurnGunRightRadians(getHeadingRadians() + (getRadarHeadingRadians() - getGunHeadingRadians()));
-//		setTurnGunRightRadians(Utils.normalRelativeAngle(radarTurn));
-//		System.out.println("Found a robot!");
-
-//		fire(1);
+		setTurnGunRightRadians((getRadarHeadingRadians() - getGunHeadingRadians()));
+		fire(1);
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-//		back(10);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
 		back(20);
 	}
-
-
 }
-								
